093bdf848b60b08b1aa4ef56fa5a97c86c4d8b5d
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-17 15:56:20 +0530

    uc3: rest call path var.

--- src/main/java/com/HelloMessagingAppController/HelloWorldController.java
+++ src/main/java/com/HelloMessagingAppController/HelloWorldController.java
@@ -1,20 +1,26 @@
 package com.HelloMessagingAppController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
 
-    // GET Request :- Rest call: curl localhost:8080/hello -w "\n"
+   
     @RequestMapping(value = {"", "/", "/home"})
     public String hello(){
         return "Hello from Bridgelabz";
     }
-}
+
+    
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String hello(@RequestParam(value = "name") String name){
         return "Hello " + name + " !!";
+    }
+
+
+    @GetMapping("/param/{name}")
+    public String helloParam(@PathVariable String name){
+        return "Hello " + name +" !!";
     }
 }
