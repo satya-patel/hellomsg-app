60687a0082581e54605dd4904fa0d238084a0b57
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-17 16:01:27 +0530

    uc4: post req.

--- src/main/java/com/HelloMessagingAppController/HelloWorldController.java
+++ src/main/java/com/HelloMessagingAppController/HelloWorldController.java
@@ -1,26 +1,33 @@
 package com.HelloMessagingAppController;
 
 import org.springframework.web.bind.annotation.*;
+import com.HelloMessagingAppModel.User;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
 
-   
+    
     @RequestMapping(value = {"", "/", "/home"})
     public String hello(){
         return "Hello from Bridgelabz";
     }
 
-    
+   
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String hello(@RequestParam(value = "name") String name){
         return "Hello " + name + " !!";
     }
 
-
+   
     @GetMapping("/param/{name}")
     public String helloParam(@PathVariable String name){
         return "Hello " + name +" !!";
+    }
+
+ 
+    @PostMapping("/post")
+    public String hello(@RequestBody User user){
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " !!";
     }
 }
